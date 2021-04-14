56cea839ed29bd5c3becda33f6ffbb0f13e98750
Shaman739 <Shaman739@mail.ru>
2021-04-14 21:01:47 +0400

    [*] Добавлен декоратор синхронизации Fltas в House

--- 1Example/BLL.Common/House/HouseBLL.cs
+++ 1Example/BLL.Common/House/HouseBLL.cs
@@ -3,6 +3,10 @@ using BLL.Common.House.Validate;
 using Core.Data.Domain;
 using Shamdev.TOA.BLL;
 using Shamdev.TOA.BLL.Infrastructure;
+using Shamdev.TOA.BLL.Infrastructure.ParamOfCRUD;
+using Shamdev.TOA.BLL.Infrastructure.PrepareItemForCRUDOperations.Interface;
+using Shamdev.TOA.BLL.Interface;
+using Shamdev.TOA.BLL.PrepareItemForCRUDOperations.Utils;
 using Shamdev.TOA.BLL.Validate.Interface;
 using Shamdev.TOA.DAL.Interface;
 using System;
@@ -13,11 +17,11 @@ namespace BLL.Common.House
 {
     public class HouseBLL : DefaultCRUDBLL<Core.Data.Domain.House>
     {
-        public HouseBLL(IUnitOfWork contextDB) : base(contextDB)
+        public HouseBLL(IUnitOfWork contextDB, IUserContext userContext) : base(contextDB, userContext)
         {
             //Заменяем стратегию добавления, так как нужно сохранять с flats
-            PrepareItemForCRUDStrategyFactory.Value.ReplaceStrategy(ExecuteTypeConstCRUD.ADD,new HouseAddPrepareItemForCRUDStrategy(contextDB));
-            PrepareItemForCRUDStrategyFactory.Value.ReplaceStrategy(ExecuteTypeConstCRUD.EDIT, new HouseEditPrepareItemForCRUDStrategy(contextDB));
+            PrepareItemForCRUDStrategyFactory.Value.ReplaceStrategy(ExecuteTypeConstCRUD.ADD, new FlatsSynchonizeDecorator(PrepareItemForCRUDStrategyFactory.Value.GetStrategy(ExecuteTypeConstCRUD.ADD), new FetchDomainData<Flat>(contextDB), new DefaultCRUDBLL<Flat>(contextDB, userContext)));
+            PrepareItemForCRUDStrategyFactory.Value.ReplaceStrategy(ExecuteTypeConstCRUD.EDIT, new FlatsSynchonizeDecorator(PrepareItemForCRUDStrategyFactory.Value.GetStrategy(ExecuteTypeConstCRUD.EDIT), new FetchDomainData<Flat>(contextDB), new DefaultCRUDBLL<Flat>(contextDB, userContext)));
 
             ValidateDomainObject.Value.AddStrategy(ValidateTypeConstCRUD.ADD_OR_EDIT, new Lazy<IValidateDomainObject<Core.Data.Domain.House>>(() => new HouseValidateDomainObject()));
         }
---
+++ 1Example/BLL.Common/House/PrepareStrategy/FlatsSynchonizeDecorator.cs
@@ -0,0 +1,48 @@
+﻿using Core.Data.Domain;
+using Shamdev.TOA.BLL.Infrastructure.ParamOfCRUD;
+using Shamdev.TOA.BLL.Infrastructure.PrepareItemForCRUDOperations.Interface;
+using Shamdev.TOA.BLL.Interface;
+using Shamdev.TOA.BLL.PrepareItemForCRUDOperations.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Common.House.PrepareStrategy
+{
+
+    class FlatsSynchonizeDecorator : IPrepareItemForCRUDStrategy<Core.Data.Domain.House>
+    {
+        IPrepareItemForCRUDStrategy<Core.Data.Domain.House> _prepareItemForCRUDStrategy;
+        IDefaultCRUDBLL<Flat> _flatBLL;
+        IFetchData<Flat> _fetchFlatBLL;
+        public FlatsSynchonizeDecorator(IPrepareItemForCRUDStrategy<Core.Data.Domain.House> prepareItemForCRUDStrategy, IFetchData<Flat> fetchData, IDefaultCRUDBLL<Flat> flatBll)
+        {
+            _prepareItemForCRUDStrategy = prepareItemForCRUDStrategy;
+            _fetchFlatBLL = fetchData;
+            _flatBLL = flatBll;
+            if (_prepareItemForCRUDStrategy == null)
+                throw new ArgumentNullException("prepareItemForCRUDStrategy");
+            if (_flatBLL == null)
+                throw new ArgumentNullException("flatBLL");
+            if (_fetchFlatBLL == null)
+                throw new ArgumentNullException("fetchFlatBLL");
+
+        }
+        public Core.Data.Domain.House GetItem(DefaultParamOfCRUDOperation<Core.Data.Domain.House> item)
+        {
+            Core.Data.Domain.House house = _prepareItemForCRUDStrategy.GetItem(item);
+
+            if (house != null)
+            {
+                if (house.Flats == null)
+                    house.Flats = new List<Flat>();
+
+                SynchronizeChildrenObject<Flat> synchronizeChildrenObject = new SynchronizeChildrenObject<Flat>(house.Flats, _flatBLL, _fetchFlatBLL);
+                synchronizeChildrenObject.Synchonize(item.Item.Flats);
+            }
+            return house;
+        }
+    }
+}
